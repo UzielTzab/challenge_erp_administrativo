2023141ed942b91470fcde374d82f2e27cdaf48e
uzieltzab <uzielruxtion@gmail.com>
2026-09-11 23:19:04 -0600

    Add gitignore and remove generated files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ChallengeErp.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
 
